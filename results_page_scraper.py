078032fd67499bf469d70669d34b53ba8e2aa06b
Yatharth Bisht <vasub0723@gmail.com>
2025-11-15 02:45:47 +0530

    results page updated

--- results_page_scraper.py
+++ results_page_scraper.py
@@ -16,17 +16,52 @@ def run(playwright):
     page = browser.new_page()
 
     #page.goto("https://www.google.com/")
-    page.goto("https://www.screener.in/company/504180/#quarters")
+    page.goto("https://www.screener.in/company/530439/#quarters")
 
     time.sleep(3)
-    page.evaluate("window.scrollTo(0, 0)")
+    
      
 
 
     ### values needed to figure out :: 
     #  quarterly result -->current + past 4 
+    sales = [1,2,3,4,5]
+
+    ## instead of this do : last se minus 4 tak till available 
+    sales_xp_last = 0
+    for i in range(0,30):
+        curr_xp = f"/html/body/main/section[4]/div[2]/table/tbody/tr[1]/td[{i}]"
+
+        #print("this is curr xp : " , curr_xp)
+        check = page.query_selector(f"xpath={curr_xp}")
+        #print("this is check : " , check)
+        if(check):
+            sales_xp_last = i
+            # print(sales_xp_last)
+        
+            ##end mil gya 
+    
+
+    for i in range(0,5):
+        #print("this is simple I " , i)
+        temp = f"/html/body/main/section[4]/div[2]/table/tbody/tr[1]/td[{sales_xp_last-i}]"
+        #print("## this is temp : : " , temp)
+        check = page.query_selector(f"xpath = {temp}")
+        #print("this is check : " , check)
+        if(check):
+            sales[i] = page.text_content(f"xpath={temp}")
+            #print(page.text_content(f"xpath={temp}"))
+
+    print(sales)
+
+
+    
+
     #  borrowing 
+   
+
     #  marketcap 
+    page.evaluate("window.scrollTo(0, 0)")
     #  other income , profit --> current + past 4 
     #  stock PE , industry PE  {ADD LATER : MEDIAN PE IN CASE INDUSTRY PE IS NOT GIVEN}
     #  working capital days -->current and last quarter 
@@ -34,7 +69,7 @@ def run(playwright):
 
     ## perform ops , compile into final list 
 
-    spreadsheet_rows(final_list)
+    #spreadsheet_rows(final_list)
 
 
     ### scroll accordingly 
